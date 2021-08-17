e3ff03d616da2a06c609f1d03b596264c4a267ec
PanChien <zoo0605@hotmail.com>
2021-08-17 10:07:50 +0800

    add new event

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
+
+# 載入linebot.models的功能，如要增加StickerSendMessage功能，就要加進去
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
 )
 
 app = Flask(__name__)
@@ -39,9 +41,32 @@ def callback():
 # 上面的程式，會再觸發下面的程式
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text # 使用者所傳的文字
+    r = '後看不懂你說什麼?!' # 預設要回的文字
+
+    if '給我貼圖' in msg:
+        sticker_message = StickerSendMessage(
+            package_id='11537',
+            sticker_id='52002735'
+        )
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            sticker_message)
+        return
+
+    if msg in ['hi','Hi','HI']:
+        r = '嗨!! 你好哦'
+    elif msg == '你吃飯了嗎':
+        r = '我還沒吃飯，都快餓死了 >_<lll'
+    elif msg == '你是誰':
+        r = '我是Chien的機器人'
+    elif '訂位' in msg:
+        r = '你想訂位嗎? 要訂幾點的呢?'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)) # 回傳使用者的文字
+        TextSendMessage(text=r)) # 回傳使用者的文字
 
 
 if __name__ == "__main__": # 當直接被執行時，就執行以下程式
